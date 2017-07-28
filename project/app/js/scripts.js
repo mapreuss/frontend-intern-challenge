6f9f36f2723f397375abe5f764817fd36bae40b5
Marta Preuss <marta.preuss@gmail.com>
2017-07-27 23:29:51 -0300

    Send link effect

--- project/app/js/scripts.js
+++ project/app/js/scripts.js
@@ -1,24 +1,45 @@
 $(document).ready(function(){
     
-    // Hide and show label
-    hideLabel();
-    
+    // Hide and show label, when press a letter or paste an URL
+    $("#longURL").one("keypress", function(){ hideLabel() });
+    $("#longURL").bind("paste", function(){ hideLabel() });
+
+    // Result
+    $("#formURL").submit(function(e){
+        e.preventDefault();
+        result();
+    });
+
 });
 
 
 function hideLabel(){
-    $("#longURL").one("keypress", function () {
-        $("#labelLongURL").hide();
-        showLabel();
-    });
+    $("#labelLongURL").hide();
+    $("#longURL").on("blur", function(){ showLabel(); });
 }
 
 function showLabel(){
-    $("#longURL").on("blur", function(){
-        var inputSize = $("#longURL").val().length;
-        if(inputSize === 0){
-            $("#labelLongURL").show();
-            hideLabel();
-        }
-    });
+    var inputSize = $("#longURL").val().length;
+    if(inputSize === 0){
+        $("#labelLongURL").show();
+    }
+}
+
+function result(){
+    $("#submitButton").animate({ color: 'red' }, 500);
+    
+    $("#submitButton").text("Copiar");
+    
+    
+    $("#longURL").addClass("result");
+    $("#longURL").val("http://chr.dc/xyzxyz");
+    $("#resetButton").fadeIn(350).on("click", function() {reset()});
+}
+
+function reset(){
+    $("#resetButton").fadeOut(350);
+    $("#longURL").removeClass("result"); 
+    $("button").text("Encurtar");
+    $("#longURL").val("");
+    showLabel();
 }
